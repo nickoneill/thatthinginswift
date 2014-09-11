fc433621ee6e0ef3cbc2958550eccaf994a4b5e7
Nick O'Neill <nick.oneill@gmail.com>
2014-09-10 20:00:55 -0700

    hugo .12 works

--- develop.sh
+++ develop.sh
@@ -2,4 +2,4 @@
 
 # start the server with our modified herring cove theme
 # watch for changes
-hugo_0.11_darwin_amd64 server --theme=herring-cove-swift -w
+hugo server --theme=herring-cove-swift -w
